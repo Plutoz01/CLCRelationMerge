bb420bcd27d1fedca719db9ee8570c04cccfacb8
Plutoz01 <plutoz01@gmail.com>
2012-07-24 20:18:19 +0200

    Uploading source

---
+++ ClcIdRelationEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GraphTools.OSM;
+
+namespace CLCRelationMerge
+{
+    public class ClcIdRelationEqualityComparer : IEqualityComparer<OSMRelation>
+    {
+        public bool  Equals(OSMRelation x, OSMRelation y)
+        {
+ 	        return x.Tags[Worker.CLCid].Equals(y.Tags[Worker.CLCid]);
+        }
+
+        public int  GetHashCode(OSMRelation obj)
+        {
+ 	        return obj.Tags[Worker.CLCid].GetHashCode();
+        }
+    }
+}
